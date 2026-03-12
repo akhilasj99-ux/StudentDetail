8da2330b900b3ba15eab2373ed2f115a42c4b5fd
AKHILA S J <akhilasj99@gmail.com>
2026-03-12 11:52:45 +0530

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Student (
+    roll_no INT PRIMARY KEY,
+    name VARCHAR(50),
+    course_name VARCHAR(50),
+    m1 INT,
+    m2 INT,
+    m3 INT,
+    total INT,
+    average DOUBLE,
+    result VARCHAR(10)
+);
